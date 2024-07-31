7a774d0eff56154da0b7a3036c161ee8c9d1fe54
Joy O'Halloran <johalloran@protonmail.com>
2024-07-31 15:08:57 -0700

    added spots model, migration, and seeder

---
+++ backend/db/seeders/20240731220046-add-spots.js
@@ -0,0 +1,57 @@
+'use strict';
+const { Spot } = require('../models');
+
+const spots = [
+  {
+    "address": "123 Disney Lane",
+    "city": "San Francisco",
+    "state": "California",
+    "country": "United States of America",
+    "lat": 37.7645358,
+    "lng": -122.4730327,
+    "name": "App Academy",
+    "description": "Place where web developers are created",
+    "price": 123,
+  },
+  {
+    "address": "43 Road Way",
+    "city": "Boston",
+    "state": "MA",
+    "country": "USA",
+    "lat": 50.8999,
+    "lng": 101.22213,
+    "name": "New apartment",
+    "description": "Come live here for a week",
+    "price": 50.89,
+  },
+  {
+    "address": "2 Main Street, Apt. #4",
+    "city": "Dallas",
+    "state": "Texas",
+    "country": "United States of America",
+    "lat": -70.20490234,
+    "lng": -21.2222,
+    "name": "Another apartment",
+    "description": "Live here",
+    "price": 100.99,
+  },
+
+]
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    spots.forEach(async spot => {
+      await Spot.create(spot)
+    })
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    spots.forEach(async spot => {
+      const instance = await Spot.findOne({where: spot})
+      await instance.destroy();
+    })
+  }
+};
