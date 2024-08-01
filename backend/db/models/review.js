3fa6e420f7ed206094ba6637bd5802a438defdd8
Aaliyah Harper <aeverly14@pm.me>
2024-08-01 18:54:31 -0400

    updated get all spots to include previewImage and avgRating

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -9,9 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Review.belongsTo(models.Spot, { foreignKey: 'spotId' });
-
       Review.belongsTo(models.User, { foreignKey: 'userId' });
-
       Review.hasMany(models.ReviewImage, { foreignKey: 'reviewId' });
     }
   }
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-const { Spot, Sequelize } = require('../../db/models');
+const { Spot, Review, SpotImage, Sequelize } = require('../../db/models');
 
 router.get('/', async (req, res, next) => {
   try {
-    const spots = await Spot.findAll();
+    const spots = await Spot.findAll({
+      include: [
+        {
+          model: Review,
+        },
+        {
+          model: SpotImage,
+        }
+      ],
+    });
+
+    for (const spot of spots) {
+      let totalStars = 0;
+      spot.Reviews.forEach(review => {
+        totalStars += review.stars;
+      });
+
+      const avgRating = totalStars / spot.Reviews.length;
+      spot.dataValues.avgRating = avgRating;
+
+      const previewImages = spot.SpotImages.find(image => image.preview);
+      spot.dataValues.previewImage = previewImages.url;
+
+      spot.dataValues.Reviews = undefined;
+      spot.dataValues.SpotImages = undefined;
+    }
+
     res.json({
       Spots: spots,
     });
@@ -32,9 +58,9 @@ router.post('/', async (req, res, next) => {
 
     res.status(201).json(newSpot);
   } catch (err) {
-    if ( err instanceof Sequelize.ValidationError) {
+    if (err instanceof Sequelize.ValidationError) {
       res.status(400).json({
-        message: err.message
+        message: err.message,
       });
     }
   }
