83b78de7de0f830fd96a2df1a0e412b660a9474a
Aaliyah Harper <aeverly14@pm.me>
2024-08-01 19:34:19 -0400

    finished get spot by id route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,13 +3,32 @@ const router = express.Router();
 
 const { Spot, Review, SpotImage, Sequelize, User } = require('../../db/models');
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:spotId', async (req, res, next) => {
   try {
-    const { id } = req.params;
-    let spot = await Spot.findByPk(id, {
-      include: [{ model: SpotImage }, { model: Review }, { model: User }],
+    const { spotId } = req.params;
+    let spot = await Spot.findByPk(spotId, {
+      include: [
+        {
+          model: SpotImage,
+        },
+        {
+          model: Review,
+          attributes: ['stars'],
+        },
+        {
+          model: User,
+          attributes: ['id', 'firstName', 'lastName'],
+        },
+      ],
     });
-    console.log(spot);
+
+    if (!spot) {
+      return res.status(404).json({
+        message: "Spot couldn't be found",
+      });
+    }
+
+    spot = spot.toJSON();
 
     let totalStars = 0;
     if (spot.Reviews.length) {
@@ -18,13 +37,53 @@ router.get('/:id', async (req, res, next) => {
       });
 
       const avgRating = totalStars / spot.Reviews.length;
-      spot.dataValues.avgRating = avgRating;
+      spot.avgStarRating = avgRating;
     }
 
-    spot = spot.toJSON();
+    spot.Owner = spot.User;
+    spot.numReviews = spot.Reviews.length;
     delete spot.Reviews;
+    delete spot.User;
 
-    res.json(spot);
+    const {
+      id,
+      ownerId,
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price,
+      createdAt,
+      updatedAt,
+      numReviews,
+      avgStarRating,
+      SpotImages,
+      Owner,
+    } = spot;
+
+    res.json({
+      id,
+      ownerId,
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price,
+      createdAt,
+      updatedAt,
+      numReviews,
+      avgStarRating,
+      SpotImages,
+      Owner,
+    });
   } catch (err) {
     next(err);
   }
@@ -69,17 +128,7 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    const {
-      address,
-      city,
-      state,
-      country,
-      lat,
-      lng,
-      name,
-      description,
-      price,
-    } = req.body;
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
 
     const newSpot = await Spot.create({
       address,
