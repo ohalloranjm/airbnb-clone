9acd07162dfcb8f4347ce1f7672150f537e34237
Joy O'Halloran <johalloran@protonmail.com>
2024-08-01 03:36:40 -0700

    started get spot by id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-const { Spot, Review, SpotImage, Sequelize } = require('../../db/models');
+const { Spot, Review, SpotImage, Sequelize, User } = require('../../db/models');
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    let spot = await Spot.findByPk(id, {
+      include: [{ model: SpotImage }, { model: Review }, { model: User }],
+    });
+    console.log(spot);
+
+    let totalStars = 0;
+    if (spot.Reviews.length) {
+      spot.Reviews.forEach(review => {
+        totalStars += review.stars;
+      });
+
+      const avgRating = totalStars / spot.Reviews.length;
+      spot.dataValues.avgRating = avgRating;
+    }
+
+    spot = spot.toJSON();
+    delete spot.Reviews;
+
+    res.json(spot);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get('/', async (req, res, next) => {
   try {
@@ -12,7 +39,7 @@ router.get('/', async (req, res, next) => {
         },
         {
           model: SpotImage,
-        }
+        },
       ],
     });
 
@@ -42,7 +69,17 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    const {
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price,
+    } = req.body;
 
     const newSpot = await Spot.create({
       address,
