fc6d1955af512ba9db3ea499d0b635b352402afe
Aaliyah Harper <aeverly14@pm.me>
2024-08-02 13:17:53 -0400

    add get all reviews by current user route

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { Spot, Review, SpotImage, Sequelize, User } = require('../../db/models');
 
+
 router.get('/current', async (req, res, next) => {
   const userId = req.user.id;
   const reviews = await Review.findAll({
@@ -13,6 +14,5 @@ router.get('/current', async (req, res, next) => {
 
   res.json(reviews);
 });
-
 
 module.exports = router;
