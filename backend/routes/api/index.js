04a6c87ea9d32338cbc1e8f17d8531485ea32a89
Aaliyah Harper <aeverly14@pm.me>
2024-08-02 13:13:23 -0400

    created reviews route file

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const { restoreUser } = require("../../utils/auth.js");
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
+const reviewsRouter = require('./reviews.js');
 
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
@@ -13,6 +14,7 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
+router.use('/reviews', reviewsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
---
+++ backend/routes/reviews.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const { Spot, Review, SpotImage, Sequelize, User } = require('../../db/models');
+
+
+module.exports = router;
