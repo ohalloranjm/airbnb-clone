2b1209d8874fd443c3e400da8afc508bacf7426b
Aaliyah Harper <aeverly14@pm.me>
2024-08-02 14:13:15 -0400

    updated review model validation with custom msgs

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -17,13 +17,32 @@ module.exports = (sequelize, DataTypes) => {
     {
       spotId: DataTypes.INTEGER,
       userId: DataTypes.INTEGER,
-      review: DataTypes.STRING,
+      review: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            args: [true],
+            msg: 'Review text is required',
+          },
+        },
+      },
       stars: {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
-          min: 1,
-          max: 5,
+          notNull: {
+            args: [true],
+            msg: 'Stars are required'
+          },
+          min: {
+            args: [1],
+            msg: 'Stars must be an integer from 1 to 5',
+          },
+          max: {
+            args: [5],
+            msg: 'Stars must be an integer from 1 to 5',
+          },
         },
       },
     },
