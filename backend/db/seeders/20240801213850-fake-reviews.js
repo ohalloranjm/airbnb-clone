943bde04239cf89b6332c8a2251c838e80dc7a20
Aaliyah Harper <aeverly14@pm.me>
2024-08-02 18:32:21 -0400

    created better review seeds

--- backend/db/seeders/20240801213850-fake-reviews.js
+++ backend/db/seeders/20240801213850-fake-reviews.js
@@ -14,19 +14,29 @@ module.exports = {
       {
         spotId: 1,
         userId: 2,
-        review: "It's ok",
+        review:
+          "The spot was okay, but I don't think it's worth the hype. There are other places nearby that offer a similar experience at a better value.",
         stars: 3,
       },
       {
         spotId: 3,
         userId: 1,
-        review: 'Amaaazing!',
+        review:
+          "This is hands down the best place I've visited this year. The experience was outstanding and it exceeded all my expectations. Highly recommended!",
         stars: 5,
       },
       {
         spotId: 3,
         userId: 3,
-        review: 'so-so',
+        review:
+          'I really enjoyed visiting this spot. The atmosphere was great and the service was excellent. However, I think there could be some improvements in the amenities offered.',
+        stars: 4,
+      },
+      {
+        spotId: 1,
+        userId: 2,
+        review:
+          'I was initially impressed with this location, but my experience was marred by poor maintenance and cleanliness issues. I expected better for the price.',
         stars: 2,
       },
     ];
