17b0d9d2be8a4da70a5dc04f604c12c52d7dbe60
Joy O'Halloran <johalloran@protonmail.com>
2024-08-02 13:32:06 -0700

    added delete review route

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { Spot, Review, ReviewImage, Sequelize, User } = require('../../db/models');
+const {
+  Spot,
+  Review,
+  ReviewImage,
+  Sequelize,
+  User,
+} = require('../../db/models');
 
 router.get('/current', async (req, res, next) => {
   const userId = req.user.id;
@@ -60,13 +66,13 @@ router.put('/:reviewId', async (req, res, next) => {
     const review = await Review.findOne({
       where: {
         id: reviewId,
-        userId
-      }
+        userId,
+      },
     });
 
     if (!review) {
       return res.status(404).json({
-        message: "Review couldn't be found"
+        message: "Review couldn't be found",
       });
     }
 
@@ -80,10 +86,32 @@ router.put('/:reviewId', async (req, res, next) => {
         message: 'Bad Request',
         errors: {
           [err.errors[0].path]: err.errors[0].message,
-        }
+        },
       });
     }
   }
+});
+
+router.delete('/:reviewId', async (req, res, next) => {
+  const { reviewId } = req.params;
+  const userId = req.user.id;
+  const review = await Review.findOne({
+    where: {
+      userId,
+      id: reviewId,
+    },
+  });
+
+  if (!review) {
+    return res.status(404).json({
+      message: "Review couldn't be found",
+    });
+  }
+
+  await review.destroy();
+  res.json({
+    message: 'Successfully deleted',
+  });
 });
 
 module.exports = router;
