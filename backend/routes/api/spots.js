b794b6fa4699e55e4c2c11f0491d3d7d793c2d22
Joy O'Halloran <johalloran@protonmail.com>
2024-08-05 09:57:42 -0700

    finally done with post bookings

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -130,20 +130,39 @@ router.post('/:spotId/bookings', requireAuth, async (req, res) => {
               [Op.between]: [startDate, endDate],
             },
           },
+          {
+            [Op.and]: {
+              startDate: {
+                [Op.lte]: startDate,
+              },
+              endDate: {
+                [Op.gte]: endDate,
+              },
+            },
+          },
         ],
       },
     });
 
     if (conflict) {
-      const startConflict = conflict.startDate <= startDate;
-      const endConflict = conflict.endDate >= endDate;
-      return res.status(403).json({
+      const resObj = {
         message: 'Sorry, this spot is already booked for the specified dates',
         errors: {
-          startDate: startConflict ? 'Start date conflicts with an existing booking' : null,
-          endDate: endConflict ? 'End date conflicts with an existing booking' : null,
+          startConflict: 'Start date conflicts with an existing booking',
+          endConflict: 'End date conflicts with an existing booking',
         },
-      });
+      };
+
+      const endDateOverlap = endDate <= conflict.endDate;
+      const startDateOverlap = startDate >= conflict.startDate;
+
+      if (endDateOverlap && !startDateOverlap) {
+        resObj.errors.startConflict = undefined;
+      } else if (startDateOverlap && !endDateOverlap) {
+        resObj.errors.endConflict = undefined;
+      }
+
+      return res.status(403).json(resObj);
     }
 
     const newBooking = await Booking.create({
@@ -424,7 +443,17 @@ router.delete('/:spotId', requireAuth, async (req, res) => {
 
 router.post('/', requireAuth, async (req, res) => {
   try {
-    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    const {
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price,
+    } = req.body;
 
     const newSpot = await Spot.create({
       address,
