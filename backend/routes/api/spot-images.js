bd6c6870008827376d0088867109001c11b2128e
Joy O'Halloran <johalloran@protonmail.com>
2024-08-05 11:19:13 -0700

    added delete spot image route (not tested)

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const { requireAuth } = require('../../utils/auth');
+
+const {
+  Spot,
+  Booking,
+  Review,
+  ReviewImage,
+  SpotImage,
+  Sequelize,
+  User,
+} = require('../../db/models');
+
+const Op = Sequelize.Op;
+
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+  const { imageId } = req.params;
+  const image = await SpotImage.findOne({
+    where: {
+      id: imageId,
+    },
+    include: {
+      model: Spot,
+    },
+  });
+
+  if (!image) {
+    return res.status(404).json({
+      message: "Spot Image couldn't be found",
+    });
+  }
+
+  const userId = req.user.id;
+  if (image.Spot.ownerId !== userId) {
+    return res.status(403).json({
+      message: 'forbidden',
+    });
+  }
+
+  await image.destroy();
+
+  res.json({
+    message: 'Successfully deleted',
+  });
+});
+
+module.exports = router;
