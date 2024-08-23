efb94feb617301808919575bfb9998a432d9baf5
Joy O'Halloran <johalloran@protonmail.com>
2024-08-23 10:13:34 -0700

    finished frontend authme

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,21 @@
+import Cookies from 'js-cookie';
+
+export async function csrfFetch(url, options = {}) {
+  options.headers = options.headers ?? {};
+  options.method = options.method ?? 'GET';
+  if (options.method.toUpperCase() !== 'GET') {
+    options.headers['Content-Type'] =
+      options.headers['Content-Type'] ?? 'application/json';
+    options.headers['XSRF-TOKEN'] = Cookies.get('XSRF-TOKEN');
+  }
+
+  const res = await window.fetch(url, options);
+
+  if (res.status >= 400) throw res;
+  else return res;
+}
+
+// call this to get the "XSRF-TOKEN" cookie, should only be used in development
+export function restoreCSRF() {
+  return csrfFetch('/api/csrf/restore');
+}
