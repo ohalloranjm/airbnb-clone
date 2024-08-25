b0a5adc0397345aac819a13bd866326e741d157d
Joy O'Halloran <johalloran@protonmail.com>
2024-08-25 09:04:10 -0700

    finished except for css

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -12,30 +12,36 @@ export default function SpotDetails () {
     }, [dispatch])
     const spot = useSelector(state => state.spots[spotId]);
 
-    const {SpotImages} = spot;
-    const previewImage = SpotImages.find(img => img.preview);
-    const otherImages = [];
-    let i = 0;
-    while (otherImages.length < 4 && i < SpotImages.length) {
-        const img = SpotImages[i];
-        if (!img.preview) otherImages.push(img);
-        i++;
-    }
+    if (spot) {
 
-    return <>
-        <h1>{spot.name}</h1>
-        <p className="sd-location">{spot.city}, {spot.state}, {spot.country}</p>
-        <img className="sd-image-primary" src={previewImage.url} alt={spot.name} />
-        {otherImages.map(i => <img className="sd-image-secondary" src={i.url} />)}
-        <p className="sd-host">Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</p>
-        <p className="sd-description">{spot.description}</p>
-        <div className="sd-callout">
-            <p className="sd-callout-price">${round(spot.price, 2)} /night</p>
-            <button 
-                className="sd-callout-reserve" 
-                type="button"
-                onClick={e => alert('Feature coming soon!')}
-            >Reserve</button>
-        </div>
-    </>
+        const {SpotImages} = spot;
+        const previewImage = SpotImages.find(img => img.preview);
+        const otherImages = [];
+        let i = 0;
+        while (otherImages.length < 4 && i < SpotImages.length) {
+            const img = SpotImages[i];
+            if (!img.preview) otherImages.push(img);
+            i++;
+        }
+
+
+        return <>
+            <h1>{spot.name}</h1>
+            <p className="sd-location">{spot.city}, {spot.state}, {spot.country}</p>
+            <img className="sd-image-primary" src={previewImage.url} alt={spot.name} />
+            {otherImages.map(i => <img className="sd-image-secondary" src={i.url} key={i.id} />)}
+            <p className="sd-host">Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</p>
+            <p className="sd-description">{spot.description}</p>
+            <div className="sd-callout">
+                <p className="sd-callout-price">${round(spot.price, 2)} /night</p>
+                <button 
+                    className="sd-callout-reserve" 
+                    type="button"
+                    onClick={e => alert('Feature coming soon!')}
+                >Reserve</button>
+            </div>
+        </>
+    } else {
+        return <h2>Could not find spot</h2>
+    }
 }
