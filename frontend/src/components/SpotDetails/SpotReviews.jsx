f2145b33348e9ab6de4fccf22537e05c5b7a80f9
Joy O'Halloran <johalloran@protonmail.com>
2024-08-26 15:27:45 -0700

    added Post Your Review button

--- frontend/src/components/SpotDetails/SpotReviews.jsx
+++ frontend/src/components/SpotDetails/SpotReviews.jsx
@@ -12,17 +12,12 @@ export default function SpotReviews({spot, reviewInfo}) {
     const user = useSelector(state => state.session.user);
     const reviews = Object.values(allReviews).filter(r => r.spotId === spot.id).sort((a, b) => a.createdAt > b.createdAt ? -1 : 1);
 
-    const shouldPromptReview = !reviews.length 
-        && user 
-        && user.id !== spot.ownerId;
-
-    const reviewPrompt = shouldPromptReview ?
-        <p>Be the first to post a review!</p> :
-        null
-    
+    const shouldPromptReview = user && user.id !== spot.ownerId;
+        
     return <div className="spot-details-reviews">
         <h2>{reviewInfo}</h2>
+        {shouldPromptReview ? <button>Post Your Review</button> : null}
         {reviews.map(r => <ReviewTile key={r.id} review={r} />)}
-        {reviewPrompt}
+        {shouldPromptReview && !reviews.length ? <p>Be the first to post a review!</p> : null}
     </div>
 }
