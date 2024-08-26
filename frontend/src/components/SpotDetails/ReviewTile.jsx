ba4c8ba5d0a9b1a58905e4eafdbe919dc66de113
Joy O'Halloran <johalloran@protonmail.com>
2024-08-26 16:53:23 -0700

    bug not as bad, but still wonky - modal doesnt auto-close, name doesnt auto-populate

--- frontend/src/components/SpotDetails/ReviewTile.jsx
+++ frontend/src/components/SpotDetails/ReviewTile.jsx
@@ -1,11 +1,13 @@
+import { useSelector } from "react-redux";
 import { dateFromString } from "../../utils"
 
 export default function ReviewTile({review}) {
 
     const {month, year} = dateFromString(review.createdAt);
+    const defaultUser = useSelector(state => state.user);
 
     return <div className="review-tile">
-        <p>{review.User.firstName}</p>
+        <p>{review.User ? review.User.firstName : defaultUser?.firstName}</p>
         <p>{month} {year}</p>
         <p>{review.review}</p>
     </div>
