12e40727fe411dc73b81c628b2c6796c5d8e2867
Joy O'Halloran <johalloran@protonmail.com>
2024-08-27 09:14:18 -0700

    populating spot tiles

---
+++ frontend/src/components/ManageSpots/ManageSpots.jsx
@@ -0,0 +1,27 @@
+import { useDispatch, useSelector } from "react-redux"
+import { getSpots } from "../../store/spots"
+import { useEffect } from "react"
+import { Link, Navigate } from "react-router-dom";
+import SpotTile from "../SpotIndex/SpotTile";
+
+export default function ManageSpots() {
+    const user = useSelector(store => store.session.user);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getSpots());
+    }, [dispatch]);
+    const allSpots = useSelector(store => store.spots);
+
+    if (user) {
+        const spots = Object.values(allSpots).filter(s => s.ownerId === user.id)
+        return <>
+            <h1>Manage Spots</h1>
+            {spots.length ? <div className="spot-index">
+                {spots.map(spot => <SpotTile key={spot.id} spot={spot} />)}
+            </div> : <Link to="/spots/new">Create a new spot</Link>}
+        </>
+    } else {
+        return <Navigate to="/" />
+    }
+
+}
