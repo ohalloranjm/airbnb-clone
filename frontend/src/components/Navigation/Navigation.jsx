d5a755bcd25f0d1782a7ed2f53250aa7071a94eb
Joy O'Halloran <johalloran@protonmail.com>
2024-08-28 10:27:08 -0700

    updated global & navigation styling

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -8,7 +8,7 @@ function Navigation({ isLoaded }) {
 
   return (
       <nav className="navbar">
-        <NavLink to="/"><img className="nav-logo" src="/logo.png" alt="waterbnb" /> waterbnb</NavLink>
+        <NavLink to="/" className="logo-link"><img className="nav-logo" src="/logo.png" alt="waterbnb" /> waterbnb</NavLink>
         {isLoaded && (
           <ul className="navbar-right">
             {sessionUser ? <li><NavLink to="/spots/new">Create a New Spot</NavLink></li> : null}
